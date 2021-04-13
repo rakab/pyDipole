c8fcfd99a3b5fad4cf167e435b2be2a06b0252d2
Bakar Chargeishvili <bakar.chargeishvili@gmx.de>
2021-04-13 12:11:22 +0200

    Finish the calculate routine for the massless case

--- calculate.py
+++ calculate.py
@@ -1,5 +1,7 @@
 import subprocess
 from pyDipole import Process
+import sympy as sy
+import re
 
 proc = Process(['e','ebar'],['u','ubar'])
 
@@ -14,13 +16,13 @@ for par1 in proc.all_particles:
         par1_rules += '''\
 id Col({0},{0}) = CA;
 id Gamm({0}) = CA*11/6-Tr*Nfl;
-id K({0}) = (67/18-Pi^2/6)*CA-Tr*Nfl*10/9;\n
+id K({0}) = (67/18-pi^2/6)*CA-Tr*Nfl*10/9;\n
 '''.format(par1.id)
     else:
         par1_rules += '''\
 id Col({0},{0}) = CF;
 id Gamm({0}) = CF*3/2;
-id K({0}) = (7/2-Pi^2/6)*CF;
+id K({0}) = (7/2-pi^2/6)*CF;
 '''.format(par1.id)
 
     for par2 in proc.all_particles:
@@ -36,9 +38,9 @@ print(expressions)
 
 code = '''\
 #-
-on stats;
+off stats;
 
-S Alfas, Pi, eps, CF, CA, Tr, Nfl;
+S Alfas, pi, eps, CF, CA, Tr, Nfl;
 CF I, MuFactor, InvGamma, Col,Nu, Gamm, K;
 AutoDeclare I i;\n
 '''
@@ -46,9 +48,9 @@ AutoDeclare I i;\n
 code += expressions
 
 code += '''\
-\nid I(i1?,i2?,0) = -Alfas/(2*Pi)*InvGamma(1-eps)*Col(i1,i1)*Nu(i1)*Col(i1,i2)*MuFactor(i1,i2)^eps;
+\nid I(i1?,i2?,0) = -Alfas/(2*pi)*InvGamma(1-eps)*Col(i1,i1)^(-1)*Nu(i1)*Col(i1,i2)*MuFactor(i1,i2)^eps;
 
-id Nu(i1?) = Col(i1,i1)*(eps^(-2)-Pi^2/3)+Gamm(i1)*eps^(-1)+Gamm(i1)+K(i1);\n\n
+id Nu(i1?) = Col(i1,i1)*(eps^(-2)-pi^2/3)+Gamm(i1)*eps^(-1)+Gamm(i1)+K(i1);\n\n
 '''
 
 code += par1_rules
@@ -56,7 +58,7 @@ code += par1_rules
 code += '''\
 \n.sort
 Print;
-B InvGamma,Alfas,Pi,MuFactor;
+B InvGamma,Alfas,pi,MuFactor;
 .end
 '''
 
@@ -64,4 +66,30 @@ form_file_name = '/tmp/FORM_Code.frm'
 with open(form_file_name,'w') as f:
     f.write(code)
 
-subprocess.run(["form",form_file_name])
+"""
+Run FORM, read the result back in python and execute SymPy simplifications
+"""
+
+form_res = subprocess.run(["form","-f",form_file_name],capture_output=True,text=True).stdout
+form_res=form_res.replace('\n','')
+form_res=form_res.replace(' ','')
+form_res = form_res.split(';')[:-1]
+
+#Prepare SymPy variables
+InvGamma = sy.Function('InvGamma')
+MuFactor = sy.Function('MuFactor')
+Col = sy.Function('Col')
+eps = sy.Symbol('eps')
+a_,b_ = map(sy.Wild, 'ab')
+
+
+eq_dict = dict()
+for eq in form_res:
+    eq_name = re.search(r"^(.*)=",eq).group(0)[:-1]
+    eq_dict[eq_name] = []
+    eq = sy.together(sy.sympify(eq.replace(eq_name+'=','')))
+    eq = eq.replace(InvGamma(a_),1/sy.gamma(a_))
+    series = sy.series(eq,eps,n=0)
+    eq_dict[eq_name].append(eq)
+    eq_dict[eq_name].append(series.coeff(eps**-2))
+    eq_dict[eq_name].append(series.coeff(eps**-1))
