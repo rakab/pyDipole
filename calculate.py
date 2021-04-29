3d2eaa0485123a5595e684df1d6aa28304a42b04
Bakar Chargeishvili <bakar.chargeishvili@gmx.de>
2021-04-29 18:36:25 +0200

    Implement eq. 6.16

--- calculate.py
+++ calculate.py
@@ -8,10 +8,13 @@ from pyDipole import Process
 logging.getLogger().setLevel(logging.INFO)
 
 
-proc = Process(['e','ebar'],['t','ubar'])
+proc = Process(['e','ebar'],['t','tbar'])
 
 expressions = ''
 par1_rules = ''
+par1_defs = 'S '
+nu_rules = ''
+gamma6_rules = ''
 
 #SymPy variables
 alphas = sy.Symbol('alphas')
@@ -26,14 +29,14 @@ Nu = sy.Function('Nu')
 def eq_c27(id1, id2):
     global expressions
     expressions += '''\
-L I{0}{1} = -Alfas/(2*pi)*InvGamma(1-eps)*denom(Col({0},{0}))*Nu0({0})*Col({0},{1})*Mu0({0},{1})^eps;\n
+L I{0}{1} = -Alfas/(2*pi)*InvGamma(1-eps)*Denom(Col({0},{0}))*Nu0({0})*Col({0},{1})*Mu0({0},{1})^eps;\n
 '''.format(id1,id2)
 
 def eq_616(id1,id2):
     global expressions
     expressions += '''\
-L I{0}{1} = -Alfas*(4*pi)**eps/(2*pi)*InvGamma(1-eps)*denom(Col({0},{0}))*(Col({0},{1})*Col({1},{1})*Mu616({0},{1})^eps*
-(Nu616({0},{1})-pi^2/3)+Gamma616({0})+Gamma({0})*Log(Mu616({0},{1}))+Gamm({0})+K({0}));
+L I{0}{1} = -Alfas*(4*pi)**eps/(2*pi)*InvGamma(1-eps)*Denom(Col({0},{0}))*Col({0},{1})*(Col({0},{0})*(mu^2*s({0},{1})^-1)^eps*
+(Nu616({0},{1})-pi^2/3)+Gamma616({0})+Gamm({0})*log(mu^2*s({0},{1})^-1)+Gamm({0})+K({0}));
 '''.format(id1,id2)
 
 def eq_652(id1,id2):
@@ -42,6 +45,8 @@ def eq_652(id1,id2):
 for par1 in proc.all_particles:
     if not par1.isQCD:
         continue
+
+    par1_defs += 'm{0}, p{0}, '.format(par1.id)
 
     if par1.name == 'g':
         par1_rules += '''\
@@ -123,6 +128,36 @@ id K({0}) = (7/2-pi^2/6)*CF;
                 #C.27
                     logging.info('(a,b)=(g,b), massless, using eq C.27')
 
+        #Nu replacement rules
+        if (par1.isMassive and par2.isMassive):
+            nu_rules += '''\
+id Nu616S({0},{1}) = Denom(v({0},{1}))*(eps^-1*log(rho({0},{1}))-
+1/4*log(rhon({0},{1},{0})^2)^2-1/4*log(rhon({0},{1},{1})^2)^2-pi^2/6+
+log(rho({0},{1}))*log(Qij({0},{1})^2/s({0},{1})));
+'''.format(par1.id, par2.id)
+        elif (par1.isMassive and not par2.isMassive):
+            nu_rules += '''\
+id Nu616S({0},{1}) = eps^-2/2+eps^-1/2*log(m({0})^2/s({0},{1}))-
+1/4*log(m({0})^2/s({0},{1}))-pi^2/12-1/2*log(m({0})^2/s({0},{1}))*log(s({0},{1})*Qij({0},{1})^-2)-
+1/2*log(m({0})^2*Qij({0},{1})^-2)*log(s({0},{1})*Qij({0},{1})^-2);
+'''.format(par1.id, par2.id)
+        elif (not par1.isMassive and par2.isMassive):
+            nu_rules += '''\
+id Nu616S({1},{0}) = eps^-2/2+eps^-1/2*log(m({0})^2/s({0},{1}))-
+1/4*log(m({0})^2/s({0},{1}))-pi^2/12-1/2*log(m({0})^2/s({0},{1}))*log(s({0},{1})*Qij({0},{1})^-2)-
+1/2*log(m({0})^2*Qij({0},{1})^-2)*log(s({0},{1})*Qij({0},{1})^-2);
+'''.format(par2.id, par1.id)
+        elif (not par1.isMassive and not par2.isMassive):
+            nu_rules += '''\
+id Nu616S({0},{1}) = eps^-2;
+'''.format(par1.id, par2.id)
+
+        #gamma616 replacement rules (singular part only)
+        if not par1.isMassive:
+            gamma6_rules += 'id Gamma616({0}) = eps^-1*Gamm({0});\n'.format(par1.id)
+        else:
+            gamma6_rules += 'id Gamma616({0}) = eps^-1*CF;\n'.format(par1.id)
+
 print(expressions)
 
 
@@ -130,28 +165,94 @@ code = '''\
 #-
 off stats;
 
-S Alfas, pi, eps, CF, CA, Tr, Nfl;
-CF denom;
+S Alfas, pi, eps, CF, CA, Tr, Nfl, mu;
+CF Denom;
 CF I,InvGamma, Col, Nu0, Nu616 Gamm, K, Gamma616;
-CF Mu0, Mu616, Log;
+CF Nu616S, Nu616Ns;
+CF Mu0, Mu616, log;
+CF Qij, s, rho, rhon, v, m;
 CF NuSmm, NuSmz, NuSzm;
 CF NuNSmm, NuNSmz, NuNSzm;
-AutoDeclare I i;\n
+CF sqrt, mun;
+AutoDeclare I i;
 '''
 
-code += expressions
+par1_defs = par1_defs[:-2]+';\n\n'
+code += par1_defs
+
+code += expressions+'\n\n'
 
 code += '''\
-id Nu0(i1?) = Col(i1,i1)*(eps^(-2)-pi^2/3)+Gamm(i1)*eps^(-1)+Gamm(i1)+K(i1);\n\n
+id Nu0(i1?) = Col(i1,i1)*(eps^(-2)-pi^2/3)+Gamm(i1)*eps^(-1)+Gamm(i1)+K(i1);
+id Nu616(?k) = Nu616Ns(?k) + Nu616S(?k);
+\n\n
 '''
+
+code += nu_rules
+
+rep_rules = '''\
+id rho(i1?,i2?) = sqrt((1-v(i1,i2))*Denom(1+v(i1,i2)));
+id rhon(i1?,i2?,i3?) = sqrt((1-v(i1,i2)+2*mun(i1,i2,i3)^2*Denom(1-mun(i1,i2,i1)^2-mun(i1,i2,i2)^2))*
+Denom(1+v(i1,i2)+2*mun(i1,i2,i3)^2*Denom(1-mun(i1,i2,i1)^2-mun(i1,i2,i2)^2)));
+
+id v(i1?,i2?) = (s(i1,i2))^(-1)*sqrt((s(i1,i2))^(2)-(4*m(i1)^2*m(i2)^2) );
+id mun(i1?,i2?,i3?) = m(i3)*Denom(Qij(i1,i2));
+id Qij(i1?,i2?) = sqrt(s(i1,i2) + m(i1)^2 + m(i2)^2);
+
+'''
+
+code += '''\
+argument log;
+{0}
+endargument;
+
+argument Denom;
+{0}
+endargument;
+
+argument sqrt;
+{0}
+endargument;
+
+argument log;
+argument sqrt;
+{0}
+endargument;
+endargument;
+
+argument log;
+argument Denom;
+{0}
+endargument;
+endargument;
+
+argument sqrt;
+argument Denom;
+{0}
+endargument;
+endargument;
+
+argument log;
+argument sqrt;
+argument Denom;
+{0}
+endargument;
+endargument;
+endargument;
+
+{0}
+\n\n
+'''.format(rep_rules)
+
+code += gamma6_rules+'\n\n'
 
 code += par1_rules
 
 code += '''\
-\nargument denom;
+\nargument Denom;
 {0}
 endargument;
-id denom(?k) = exp_(?k,-1);
+id Denom(?k) = exp_(?k,-1);
 .sort
 Print;
 B InvGamma,Alfas,pi,Mu0;
@@ -177,6 +278,7 @@ MuFactor = sy.Function('MuFactor')
 Col = sy.Function('Col')
 eps = sy.Symbol('eps')
 a_,b_ = map(sy.Wild, 'ab')
+Denom = sy.Function('Denom')
 
 
 eq_dict = dict()
@@ -185,7 +287,8 @@ for eq in form_res:
     eq_dict[eq_name] = []
     eq = sy.together(sy.sympify(eq.replace(eq_name+'=','')))
     eq = eq.replace(InvGamma(a_),1/sy.gamma(a_))
+    eq = eq.replace(Denom(a_),1/a_)
     series = sy.series(eq,eps,n=0)
     eq_dict[eq_name].append(eq)
-    eq_dict[eq_name].append(series.coeff(eps**-1))
-    eq_dict[eq_name].append(series.coeff(eps**-2))
+    eq_dict[eq_name].append(series.coeff(eps**-1).simplify())
+    eq_dict[eq_name].append(series.coeff(eps**-2).simplify())
